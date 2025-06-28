0dbe0810dc7439c6bfd8418f3c00741591d04c26
BluffSet7340 <sbm7640@outlook.com>
2025-06-28 14:24:16 +0400

    completed the backend and test

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-Using the defer boolean flag allowed me to actually read the input text and log it to the console. Rather than stopping the parsing of HTML the moment it hits the script tag and starts executing it, 
+### Notes
+
+Using the defer boolean flag allowed me to actually read the input text and log it to the console. Rather than stopping the parsing of HTML the moment it hits the script tag and starts executing it, it first allows the entire DOM to be loaded entirely and then executes the content of the script. 
+
+I used the spread operator to make a copy of the array in a new memory location. Without using the spread operator, it was simply comparing the references, and since str1 and str2 point to the same string object, the branch that says "not a palindrome" will never run.
+
+Another thing that I just learnt is that leaving the variable assignment outside of any funtions in the script file means that it only runs once and since the input field is intially empty, the input variable will always hold an empty string. Keeping it in a function makes more sense as by the time the button is clicked, the user will have already typed something into the input field.
+
+I used ChatGPT to generate a regular expression that keeps only alphanumeric characters.
+
+To make the height of the div box dynamic I used min-height rather than height so that when the output is printed, the height of the div box increases to accomodate it.
+
--- script.js
+++ script.js
@@ -1,11 +1,48 @@
 const inputText = document.getElementById("text-input");
 const checkButton = document.getElementById("check-btn");
+const resultText = document.getElementById("result");
+
 
 function checkInput(){
-    const input = inputText.value;
+    // process the input text, by removing all non alphanumeric characters
+    let unprocessed_input = inputText.value;
+    
+    // use regular expression to remove the un-needed characters
+    const input = unprocessed_input.replace(/[^0-9a-zA-Z]/g, "");
+
+
     if(!input){
         alert("Please input a value");
     }
-    // alert(input);
-    // console.log(input);
+    else if(input.length === 1){
+        resultText.innerHTML= `${input} is a palindrome.`;
+    }
+    else if(input.length >= 1){
+        let str1 = input.toLowerCase().split("");
+        console.log(str1);
+        let str2 = [...str1].reverse(); //using the spread operator to make a copy
+        console.log(str2)
+
+        for(let i=0; i < input.length; i++){
+            if(str1[i]!==str2[i])
+            {
+                return false;
+                // resultText.innerHTML = `${input} is not a palidrome.`;
+            }
+        };
+        return true;
+    }
+}
+
+function printOutput(){
+    const input = inputText.value;
+    const flag = checkInput();
+    console.log(flag);
+    inputText.value ="";
+
+    if(flag === true ){
+        resultText.innerHTML = `${input} is a palindrome.`;
+    } else if(flag === false){
+        resultText.innerHTML = `${input} is not a palindrome.`;
+    }
 }
